a307f497ab1d682476ccbac084c54887a8e59e84
Andre Davidian <davidianaa@masters.edu>
2023-12-13 22:58:12 +0000

    Added edit button to card set card index;

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "cards#index"
+  root "card_sets#index"
 end
